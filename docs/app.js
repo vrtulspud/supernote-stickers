d62909875f0dec16373a609b8d3c21983adfb51b
vrtulspud <jsaga@hotmail.com>
2026-05-24 00:23:30 -0500

    Update app.js

--- docs/app.js
+++ docs/app.js
@@ -461,7 +461,7 @@ const TrailsBuilder = {
    * @param {number} screenH   Screen height
    * @returns {Uint8Array}  Stroke data bytes
    */
-  _buildStroke(contourPts, strokeNb, device, screenW, screenH, stickerWidth = 512, xOffset = null, yOffset = 10) {
+  _buildStroke(contourPts, strokeNb, device, screenW, screenH, stickerWidth = 512, xOffset = null, yOffset = 100) {
     // Dense vector points for pen trajectory (firmware needs many points)
     const vectorPts = this._interpolateContour(contourPts, 2.0);
     const nVec = vectorPts.length;
